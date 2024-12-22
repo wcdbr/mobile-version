fbee41caf24267513f3ee7c8f3f683884b6427df
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2024-12-22 10:21:19 -0300

    cleaning

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
+// imports
+import styled from 'styled-components/native'
+import { Dimensions } from 'react-native'
+
+// config
+const { width } = Dimensions.get('screen')
+
+// function
 export default function App() {
   return (
-    <View>
-    </View>
-  );
+    <AppWrapper>
+      {/* Outros conteúdos aqui */}
+    </AppWrapper>
+  )
 }
+
+// styles
+const AppWrapper = styled.View`
+  width: ${width}px;
+`
