f79f9b8f4c28c4afa9a19037a1902f37a094457f
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2024-12-23 12:41:22 -0300

    started main carousel

---
+++ src/data/MainCarouselData.jsx
@@ -0,0 +1,15 @@
+
+export const MainCarouselData = [
+    {
+        title: 'Conheça nossa família de produtos Milnutri',
+        image: require('../../assets/images/MainCarousel/main-carousel-1.png')
+    },
+    {
+        title: '2',
+        image: require('../../assets/images/MainCarousel/main-carousel-1.png')
+    },
+    {
+        title: '3',
+        image: require('../../assets/images/MainCarousel/main-carousel-1.png')
+    }
+]
